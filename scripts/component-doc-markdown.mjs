b369c0ba1756aedefd117166b64d40f53e4c7332
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2026-06-16 21:30:14 +0200

    update eslint rule (#24)

--- scripts/component-doc-markdown.mjs
+++ scripts/component-doc-markdown.mjs
@@ -1,46 +1,46 @@
 export function code(value) {
-  return `\`${escapeTableCell(value)}\``
+    return `\`${escapeTableCell(value)}\``
 }
 
 export function escapeTableCell(value) {
-  return String(value).replaceAll('|', '\\|').replaceAll('\n', '<br>')
+    return String(value).replaceAll('|', '\\|').replaceAll('\n', '<br>')
 }
 
 export function formatType(type) {
-  return type
-    .replace(/\s+/g, ' ')
-    .replaceAll(' | undefined', '')
-    .replaceAll('undefined | ', '')
+    return type
+        .replace(/\s+/g, ' ')
+        .replaceAll(' | undefined', '')
+        .replaceAll('undefined | ', '')
 }
 
 export function formatTypeCell(type, typeLinks) {
-  const formattedType = formatType(type)
-  const escapedType = escapeHtmlTags(escapeTableCell(formattedType))
-  const linkedType = linkKnownTypes(escapedType, typeLinks)
+    const formattedType = formatType(type)
+    const escapedType = escapeHtmlTags(escapeTableCell(formattedType))
+    const linkedType = linkKnownTypes(escapedType, typeLinks)
 
-  if (linkedType !== escapedType) {
-    return `<code>${linkedType}</code>`
-  }
+    if (linkedType !== escapedType) {
+        return `<code>${linkedType}</code>`
+    }
 
-  return code(formattedType)
+    return code(formattedType)
 }
 
 function escapeHtmlTags(value) {
-  return value.replaceAll('<', '&lt;').replaceAll('>', '&gt;')
+    return value.replaceAll('<', '&lt;').replaceAll('>', '&gt;')
 }
 
 function linkKnownTypes(type, typeLinks) {
-  return type.replace(/\b[A-Z][A-Za-z0-9_]*\b/g, (typeName) => {
-    const link = typeLinks[typeName]
+    return type.replace(/\b[A-Z][A-Za-z0-9_]*\b/g, (typeName) => {
+        const link = typeLinks[typeName]
 
-    if (link === undefined) {
-      return typeName
-    }
+        if (link === undefined) {
+            return typeName
+        }
 
-    return `<a href="${toRelativeLink(link)}">${typeName}</a>`
-  })
+        return `<a href="${toRelativeLink(link)}">${typeName}</a>`
+    })
 }
 
 function toRelativeLink(link) {
-  return link.replace(/^\//, '../')
+    return link.replace(/^\//, '../')
 }
